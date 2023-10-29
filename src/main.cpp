57781b9b1441536afa12d0c2472926b1915f8f3d
Just-Jasmine <doyoulikejas64@gmail.com>
2023-10-29 17:08:11 -0400

    Finished Input Function

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,27 @@
 #include "raylib.h"
 
+Vector2 getInput() {
+    Vector2 input = Vector2{0.0, 0.0};
+    if (IsKeyDown(KEY_W)) input.y -= 2.0;
+    if (IsKeyDown(KEY_A)) input.x -= 2.0;
+    if (IsKeyDown(KEY_S)) input.y += 2.0;
+    if (IsKeyDown(KEY_D)) input.x += 2.0;
+    return input;
+}
+
 int main(void) {
     InitWindow(800, 450, "Moving Circle");
 
     SetTargetFPS(60);
 
+    Vector2 ballPosition = Vector2{400.0, 225.0};
+
     while (!WindowShouldClose()){
+        ballPosition.x += getInput().x;
+        ballPosition.y += getInput().y;
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            DrawCircle(400, 225, 60, RED);
+            DrawCircle(ballPosition.x, ballPosition.y, 60, RED);
         EndDrawing();
     }
     CloseWindow();
